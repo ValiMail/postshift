d411b495578b4965b3df43b19b7a176209426a13
Dave Krupinski <david.krupinski@valimail.com>
2017-05-19 15:08:03 -0700

    Additional primary key spec coverage

--- spec/db/migrate/3_create_primary_key_tests_table.rb
+++ spec/db/migrate/3_create_primary_key_tests_table.rb
@@ -3,5 +3,13 @@ class CreatePrimaryKeyTestsTable < ActiveRecord::Migration[5.0]
     create_table 'primary_key_tests' do |t|
       t.string :name
     end
+
+    create_table 'no_primary_key_tests', id: false do |t|
+      t.string :name
+    end
+
+    create_table 'custom_primary_key_tests', id: false do |t|
+      t.primary_key :for_me
+    end
   end
 end
--- spec/models/primary_key_spec.rb
+++ spec/models/primary_key_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 class PrimaryKeyTest < ActiveRecord::Base
 end
 
+class NoPrimaryKeyTest < ActiveRecord::Base
+end
+
+class CustomPrimaryKeyTest < ActiveRecord::Base
+end
+
 FactoryGirl.define do
   factory :primary_key_test do
   end
@@ -13,11 +19,37 @@ RSpec.describe PrimaryKeyTest, type: :model do
   subject { create(:primary_key_test, name: 'pk-test') }
   before  { subject }
 
+  describe '.primary_key' do
+    it 'is the default (id) primary key' do
+      expect(described_class.primary_key).to eq 'id'
+    end
+  end
+
   it 'does not set #id on creation' do
     expect(subject.id).to be_nil
   end
 
   it 'does have one set in DB' do
     expect(described_class.find_by(name: 'pk-test').id).to be_a Integer
+  end
+end
+
+RSpec.describe NoPrimaryKeyTest, type: :model do
+  before  { ARTest.connect }
+
+  describe '.primary_key' do
+    it 'does not have one' do
+      expect(described_class.primary_key).to be_nil
+    end
+  end
+end
+
+RSpec.describe CustomPrimaryKeyTest, type: :model do
+  before  { ARTest.connect }
+
+  describe '.primary_key' do
+    it 'identifies the custom column' do
+      expect(described_class.primary_key).to eq 'for_me'
+    end
   end
 end
